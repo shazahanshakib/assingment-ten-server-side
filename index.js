7347d05724431088dd2bdc45a488f180349edf81
shazahanshakib <shazahanshakib3@gmail.com>
2025-02-15 23:23:46 +0600

    add delete allvisa

--- index.js
+++ index.js
@@ -110,12 +110,7 @@ async function run() {
       const result = await visaApplyCollection.deleteOne(query)
       res.send(result)
     })
-    // app.delete('/allvisa/:id', async(req, res)=>{
-    //   const id = req.params.id
-    //   const query = {_id: new ObjectId(id)}
-    //   const result = await visaCollection.deleteOne(query)
-    //   res.send(result)
-    // })
+   
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
