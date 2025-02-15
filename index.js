98f4ca9dbcfe585ca4c22fe320b3c1bbdc85ccf8
shazahanshakib <shazahanshakib3@gmail.com>
2025-02-15 23:22:18 +0600

    add delete of visaapply

--- index.js
+++ index.js
@@ -104,12 +104,12 @@ async function run() {
     })
     // const newVisa= {countryName, countryImage, visaType, age, fee, description, Validity, appMethod, processingTime, requiredDocuments, userEmail}
 
-    // app.delete('/visaapply/:id', async(req, res)=>{
-    //   const id = req.params.id
-    //   const query = {_id: new ObjectId(id)}
-    //   const result = await visaApplyCollection.deleteOne(query)
-    //   res.send(result)
-    // })
+    app.delete('/visaapply/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await visaApplyCollection.deleteOne(query)
+      res.send(result)
+    })
     // app.delete('/allvisa/:id', async(req, res)=>{
     //   const id = req.params.id
     //   const query = {_id: new ObjectId(id)}
